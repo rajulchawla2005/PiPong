0c6d0b81a9a19bb0fe9c40bdfe6da436cb5b0593
rajulchawla2005 <rajulchawla2005@gmail.com>
2025-02-21 19:16:05 +0000

    pygame test

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("this is a test for a commit")
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+			
+	screen.fill("purple")
+	
+	pygame.display.flip()
+	
+	clock.tick(60)
+	
+pygame.quit()
