55fb9e95974c8cd1a8621dcc58f2c4240cc5cc07
rajulchawla2005 <rajulchawla2005@gmail.com>
2025-02-22 20:32:30 +0000

    working on the font, barriers and player input

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pygame
+import time
 
 # rect tuple (x-axis, y-axis, width, height)
 
 max_width = 1280
 max_height = 720
 bar_height = 60
+bounces = []
+borders = []
+
+global running, player_score, computer_score, player_rect, computer_rect
+running = True
 player_score = 0
 computer_score = 0
-
-global running
-running = True
+player_rect = None
+computer_rect = None
 
 # pygame setup
 pygame.init()
-my_font = pygame.font.SysFont('Comic Sans MS', 30)
+my_font = pygame.font.SysFont('Comic Sans MS', 100)
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 
@@ -23,26 +28,64 @@ def initialise():
 
 	# initialise the display
 	screen.fill("black")
-	#top bar
-	pygame.draw.rect(screen, "white", (0, 0, max_width, bar_height))
+	# top bar (x=0, y=0, width=max_width, height=bar_height)
+	top_rect = pygame.draw.rect(screen, "white", (0, 0, max_width, bar_height))
+	# bottom bar (x=0, y=max_height-bar_height, width=max_width, height=bar_height
+	bottom_rect = pygame.draw.rect(screen, "white", (0, max_height-bar_height, max_width, bar_height))
+	# left bar (x=0, y=0, width=bar_height/2, height=max_height)
+	left_rect = pygame.draw.rect(screen, "grey", (0, 0, bar_height/2, max_height))
+	# right bar (x=max_width-(bar_height/2), y=0, width=bar_height/2, height=max_height)
+	right_rect = pygame.draw.rect(screen, "grey", ((max_width-(bar_height/2)), 0, bar_height/2, max_height))
+	# put all the rectangles into either a bounce category or a lose category
+	bounces.append(top_rect)
+	bounces.append(bottom_rect)
+	borders.append(left_rect)
+	borders.append(right_rect)
 	
-	#bottom bar
-	pygame.draw.rect(screen, "white", (0, max_height-bar_height, max_width, bar_height))
+	# draw the player (x=bar_height/2, y=max_width/2-player_height/2, width=bar_height/2, height=150)
+	player_rect = pygame.draw.rect(screen, "white", ((bar_height/2), 285, bar_height/2, 150))
 	
-	my_font = pygame.font.SysFont('Comic Sans MS', 100, bold=True)
+	# put both scores down
+	update_score()
+	print(borders, bounces)
 	
+# will update the score to the global values, updating the screen
+def update_score():
+	# get the top rectangle (background of scores
+	top_rect = bounces.pop(0)
+	# redraw the top rectangle
+	top_rect = pygame.draw.rect(screen, "white", (0, 0, max_width, bar_height))
+	# put the rectangle back in the bounces
+	bounces.insert(0, top_rect)
+	# draw the scores 
+	player_score_text_surface = my_font.render(str(player_score), True, "black")
+	screen.blit(player_score_text_surface, (426, 0))
+	computer_score_text_surface = my_font.render(str(computer_score), True, "black")
+	screen.blit(computer_score_text_surface, (853, 0))
+	
+# increases the player score, then update the scores
+def player_score_increase():
+	global player_score
+	player_score += 1
 	update_score()
 	
+# increases the computer score, then update the scores
+def computer_score_increase():
+	global computer_score
+	computer_score += 1
+	update_score()
 
-def update_score():
-	player_score_text_surface = my_font.render(str(player_score), True, "white")
-	screen.blit(player_score_text_surface, (420, 100))
-	computer_score_text_surface = my_font.render(str(computer_score), True, "white")
-	screen.blit(computer_score_text_surface, (850, 100))
+def check_player_input():
+	# get user input
+	keys = pygame.key.get_pressed()
+	if keys[pygame.K_UP]:
+		print("up key has been pressed")
+	if keys[pygame.K_DOWN]:
+		print("down key has been pressed")
+				
 
 def update():
-	pass
-
+	check_player_input()
 
 def main():
 	initialise()
